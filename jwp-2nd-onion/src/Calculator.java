f6725afc385c2fb374d1b8e954afa05b4563ad35
nimmo-park <ggomssngma@naver.com>
2024-05-01 02:33:59 +0900

    계산기_P44

---
+++ jwp-2nd-onion/src/Calculator.java
@@ -0,0 +1,28 @@
+
+public class Calculator {
+
+	int add(int i, int j) {
+		return i + j;
+	}
+
+	int substract(int i, int j) {
+		return i - j;
+	}
+
+	int multiply(int i, int j) {
+		return i * j;
+	}
+
+	int divide(int i, int j) {
+		return i / j;
+	}
+
+	public static void main(String[] args) {
+		Calculator cal = new Calculator();
+		System.out.println(cal.add(3, 4));
+		System.out.println(cal.substract(5, 4));
+		System.out.println(cal.multiply(2, 6));
+		System.out.println(cal.divide(8, 4));
+
+	}
+}
