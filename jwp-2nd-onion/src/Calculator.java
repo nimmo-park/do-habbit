aded70e2d733de34b40fc9700af937f78a942164
nimmo-park <ggomssngma@naver.com>
2024-05-15 09:38:56 +0900

    2.2.3 테스트 코드 중복제거 p54

--- jwp-2nd-onion/src/Calculator.java
+++ jwp-2nd-onion/src/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
 		return i + j;
 	}
 
-	int substract(int i, int j) {
+	int subtract(int i, int j) {
 		return i - j;
 	}
 
@@ -20,7 +20,7 @@ public class Calculator {
 	public static void main(String[] args) {
 		Calculator cal = new Calculator();
 		System.out.println(cal.add(3, 4));
-		System.out.println(cal.substract(5, 4));
+		System.out.println(cal.subtract(5, 4));
 		System.out.println(cal.multiply(2, 6));
 		System.out.println(cal.divide(8, 4));
 
--- jwp-2nd-onion/src/CalculatorTest.java
+++ jwp-2nd-onion/src/CalculatorTest.java
@@ -1,13 +1,46 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+//더 많은 부분은 자바독 참조하기 
+//assertTrue()
+//assertFalse()
+//assertNull()
+//assertNotNull()
+//assertArrrayEquals()
 
 public class CalculatorTest {
 	
-	public static void main(String[] args) {
-		Calculator cal = new Calculator();
-		
-		System.out.println(cal.add(9, 3));
-		System.out.println(cal.substract(9, 3));
-		System.out.println(cal.multiply(9, 3));
-		System.out.println(cal.divide(9, 3));
-		
+	private Calculator cal;
+	
+	//비포 애노테이션을 사용하여서 테스트 메소드에 대한 초기화 작업을 하는것이 추후 문제 방지에 좋다.
+	@Before
+	public void setup() {
+		cal = new Calculator();
+		System.out.println("Before");
 	}
+	
+	@Test
+	public void add() {
+		//System.out.println(cal.add(6, 3));	
+		//int,long,String다양한 데이터 타입을 지원
+		assertEquals(9, cal.add(6, 3));
+		System.out.println("add");
+	}
+	
+	@Test
+	public void subtract() {
+		//System.out.println(cal.subtract(6, 3));
+		//int,long,String다양한 데이터 타입을 지원
+		assertEquals(3, cal.subtract(6, 3));
+		System.out.println("subtract");
+	}
+	
+	@After
+	public void teardown() {
+		System.out.println("teardown");
+	}
+
 }
